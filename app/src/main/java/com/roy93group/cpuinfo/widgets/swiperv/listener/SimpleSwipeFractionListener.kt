bf139f7f2d9bbb871936e7467d09300a51199109
roy <roy93group@gmail.com>
2023-07-23 11:35:06 +0700

    update

--- app/src/main/java/com/roy93group/cpuinfo/widgets/swiperv/listener/SimpleSwipeFractionListener.kt
+++ app/src/main/java/com/roy93group/cpuinfo/widgets/swiperv/listener/SimpleSwipeFractionListener.kt
@@ -1,18 +1,9 @@
-package com.roy93group.cpuinfo.widgets.swiperv.listener;
+package com.roy93group.cpuinfo.widgets.swiperv.listener
 
-import com.roy93group.cpuinfo.widgets.swiperv.SwipeMenuLayout;
+import com.roy93group.cpuinfo.widgets.swiperv.SwipeMenuLayout
 
-import kotlin.Suppress;
-
-@Suppress(names = "unused")
-public class SimpleSwipeFractionListener implements SwipeFractionListener {
-    @Override
-    public void beginMenuSwipeFraction(SwipeMenuLayout swipeMenuLayout, float fraction) {
-
-    }
-
-    @Override
-    public void endMenuSwipeFraction(SwipeMenuLayout swipeMenuLayout, float fraction) {
-
-    }
+@Suppress("unused")
+class SimpleSwipeFractionListener : SwipeFractionListener {
+    override fun beginMenuSwipeFraction(swipeMenuLayout: SwipeMenuLayout?, fraction: Float) {}
+    override fun endMenuSwipeFraction(swipeMenuLayout: SwipeMenuLayout?, fraction: Float) {}
 }
--- app/src/main/java/com/roy93group/cpuinfo/widgets/swiperv/listener/SimpleSwipeSwitchListener.kt
+++ app/src/main/java/com/roy93group/cpuinfo/widgets/swiperv/listener/SimpleSwipeSwitchListener.kt
@@ -1,29 +1,11 @@
-package com.roy93group.cpuinfo.widgets.swiperv.listener;
+package com.roy93group.cpuinfo.widgets.swiperv.listener
 
-import com.roy93group.cpuinfo.widgets.swiperv.SwipeMenuLayout;
+import com.roy93group.cpuinfo.widgets.swiperv.SwipeMenuLayout
 
-import kotlin.Suppress;
-
-@Suppress(names = "unused")
-public class SimpleSwipeSwitchListener implements SwipeSwitchListener {
-
-    @Override
-    public void beginMenuClosed(SwipeMenuLayout swipeMenuLayout) {
-
-    }
-
-    @Override
-    public void beginMenuOpened(SwipeMenuLayout swipeMenuLayout) {
-
-    }
-
-    @Override
-    public void endMenuClosed(SwipeMenuLayout swipeMenuLayout) {
-
-    }
-
-    @Override
-    public void endMenuOpened(SwipeMenuLayout swipeMenuLayout) {
-
-    }
+@Suppress("unused")
+class SimpleSwipeSwitchListener : SwipeSwitchListener {
+    override fun beginMenuClosed(swipeMenuLayout: SwipeMenuLayout?) {}
+    override fun beginMenuOpened(swipeMenuLayout: SwipeMenuLayout?) {}
+    override fun endMenuClosed(swipeMenuLayout: SwipeMenuLayout?) {}
+    override fun endMenuOpened(swipeMenuLayout: SwipeMenuLayout?) {}
 }
