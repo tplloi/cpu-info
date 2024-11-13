31daf5cc7ee38b381079bc33ee0b1b6ffa8d9891
roy <loitp@galaxyjoy.vn>
2024-11-13 11:05:16 +0700

    up

--- buildSrc/src/main/java/Libs.kt
+++ buildSrc/src/main/java/Libs.kt
@@ -51,11 +51,11 @@ object Libs {
         object Compose {
             const val compilerVersion = "1.4.8"
             const val bom = "androidx.compose:compose-bom:2023.06.01"
-            const val material = "androidx.compose.material:material"
-            const val material3 = "androidx.compose.material3:material3"
-            const val animations = "androidx.compose.animation:animation"
-            const val uiTooling = "androidx.compose.ui:ui-tooling"
-            const val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview"
+//            const val material = "androidx.compose.material:material"
+//            const val material3 = "androidx.compose.material3:material3"
+//            const val animations = "androidx.compose.animation:animation"
+//            const val uiTooling = "androidx.compose.ui:ui-tooling"
+//            const val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview"
         }
 
 //        object Test {
