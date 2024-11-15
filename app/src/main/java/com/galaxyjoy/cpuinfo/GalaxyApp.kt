006624fd7ea9342082991dd93eb972000051d8d9
roy <loitp@galaxyjoy.vn>
2024-11-15 16:22:22 +0700

    update

--- app/src/main/java/com/galaxyjoy/cpuinfo/GalaxyApp.kt
+++ app/src/main/java/com/galaxyjoy/cpuinfo/GalaxyApp.kt
@@ -9,19 +9,19 @@ import javax.inject.Inject
  * Base Application class for required initializations
  */
 
-//TODO ad applovin
-//TODO firebase
+//TODO roy93~ ad applovin
+//TODO roy93~ firebase
+//TODO roy93~ icon launcher
+//TODO roy93~rate, more, share, fb fan page, policy
+//TODO roy93~keystore
 
-//done
-//icon launcher
+//done mckimquyen
 //leak canary
 //proguard
-//rate, more, share, fb fan page, policy
 //change color primary
-//keystore
 
 @HiltAndroidApp
-class MyApp : MultiDexApplication() {
+class GalaxyApp : MultiDexApplication() {
 
     @Inject
     lateinit var initializers: InitializersApp
