6e92f76979708e69bf3c6bddb3d7a1bf56095b09
roy <loitp@galaxyjoy.vn>
2024-11-19 09:59:59 +0700

    update

--- app/src/main/java/com/galaxyjoy/cpuinfo/GalaxyApp.kt
+++ app/src/main/java/com/galaxyjoy/cpuinfo/GalaxyApp.kt
@@ -5,16 +5,29 @@ import com.galaxyjoy.cpuinfo.appinitializers.InitializersApp
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
+//TODO roy93~ why you see ad
+//TODO roy93~ vung bi mat de show applovin config
 //TODO roy93~ ad applovin
 //TODO roy93~ firebase
-//TODO roy93~ icon launcher
-//TODO roy93~rate, more, share, fb fan page, policy
-//TODO roy93~keystore
+
+//TODO roy93~ rate, more, share, fb fan page, policy
+//TODO roy93~ keystore
+//TODO roy93~ add lottie trang tri cho dep
+//TODO roy93~ them version o man hinh chu
+//TODO roy93~ splash screen
+//TODO roy93~ font scale
+//TODO roy93~ rate, more app, share app
+//TODO roy93~ github
+//TODO roy93~ license
+//TODO roy93~ gen ic_launcher https://easyappicon.com/
+//TODO roy93~ leak canary
+//TODO roy93~ proguard
+//TODO roy93~ keystore
+//TODO roy93~ rename app
 
 //done mckimquyen
 //leak canary
 //proguard
-//change color primary
 
 @HiltAndroidApp
 class GalaxyApp : MultiDexApplication() {
