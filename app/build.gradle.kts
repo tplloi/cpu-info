de9bb25dfcfe53960b0a435c56dd86360b157a01
roy <loitp@galaxyjoy.vn>
2024-12-18 15:57:05 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         minSdk = 21
         //noinspection EditedTargetSdkVersion
         targetSdk = 35
-        versionCode = 20241215
-        versionName = "2024.12.15"
+        versionCode = 20241218
+        versionName = "2024.12.18"
 
         vectorDrawables.useSupportLibrary = true
         multiDexEnabled = true
