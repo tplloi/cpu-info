5b86c30b8fac242fdfefa48901e42b4421125c28
roy <loitp@galaxyjoy.vn>
2025-01-01 13:34:51 +0700

    update

--- app/src/main/java/com/galaxyjoy/cpuinfo/GalaxyApp.kt
+++ app/src/main/java/com/galaxyjoy/cpuinfo/GalaxyApp.kt
@@ -7,13 +7,17 @@ import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 //TODO roy93~ why you see ad
-//TODO roy93~ vung bi mat de show applovin config
-//TODO roy93~ ad applovin, co the them splash screen
 //TODO roy93~ firebase
+
+//TODO roy93~ vung bi mat de show applovin config
+//TODO roy93~ splash screen
 //TODO roy93~ add lottie trang tri cho dep
 //TODO roy93~ github
 //TODO roy93~ uninstall app
+//TODO roy93~ applovin ok roi, con show ad full thi chua tim dc cho thich hop,
+//co the dung splash de show ad full?
 
+//ad applovin
 //review in app bingo
 //120hz
 //splash screen
